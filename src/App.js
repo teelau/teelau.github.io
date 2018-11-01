9f25fedc8e7875ac4c6d253f567316a47df03196
Tommy Lau <tommylau94@gmail.com>
2018-10-31 22:32:42 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import videoTeslaGrey from './assets_video/tesla-grey-outline.mp4';
 import Landing from './landing/landing';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="Main">
-        <Landing></Landing>
-        <div className="Landing">
-          <video 
-            muted
-            loop
-            autoPlay
-            src={videoTeslaGrey}
-            className="video"
-          >
-            <source src={videoTeslaGrey} type="video/mp4"/>browser not supported
-          </video>
+      <ParallaxProvider>
+        <div className="Main">
+          <Landing></Landing>
+          <div className="Landing">
+          </div>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
         </div>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-      </div>
+      </ParallaxProvider>
     );
   }
 }
--- src/landing/landing.js
+++ src/landing/landing.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import './landing.css';
+import { Parallax } from 'react-scroll-parallax';
+import VancouverMountains from './vancouver_skyline_vec.svg';
 
+const ParallaxImage = () => (
+    <Parallax
+        className="vancouver-parallax"
+        offsetYMax={0}
+        offsetYMin={-50}
+        offsetXMax={-25}
+        offsetXMin={25}
+        slowerScrollRate
+    >
+        <img src={VancouverMountains} alt="Could not display VANCOUVER_ASSET properly"/>
+    </Parallax>
+);
+  
 class Landing extends Component {
     constructor(props) {
         super(props);
@@ -32,6 +47,9 @@ class Landing extends Component {
             <div className="landing-text">
                 <h3>Tommy Lau</h3>
             </div>
+
+                {ParallaxImage()}
+
                 
             </div>
             
