cd3bc7a015333e5ffb224759550bd89572d87915
Tommy Lau <tolau@tesla.com>
2018-11-21 02:34:44 -0800

    Updates

---
+++ precache-manifest.18c19d19a731230b238f05452a3a213a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2ae88d451f728642537",
+    "url": "/static/css/main.3497c947.chunk.css"
+  },
+  {
+    "revision": "f2ae88d451f728642537",
+    "url": "/static/js/main.f2ae88d4.chunk.js"
+  },
+  {
+    "revision": "f29a51c363b04e10b221",
+    "url": "/static/js/1.f29a51c3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f2bfb71141bba43aa1c2121c9644c963",
+    "url": "/static/media/mountains.f2bfb711.svg"
+  },
+  {
+    "revision": "40531492c204aab3bf0076c02c8769c0",
+    "url": "/static/media/vancouver_skyline_vec.40531492.svg"
+  },
+  {
+    "revision": "bd0222fd64be038bb18653af15a21e2a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/teelau.github.io/precache-manifest.41945e07f8821e08e124a093a2ef3004.js"
+  "/precache-manifest.18c19d19a731230b238f05452a3a213a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/teelau.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
