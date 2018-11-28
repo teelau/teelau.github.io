66ce8325f5ef3257de937d17f4cc79fed3b6039c
Tommy Lau <tommylau94@gmail.com>
2018-11-28 02:48:54 -0800

    Updates

---
+++ precache-manifest.6e80a6116c8e1216bd81f8854e9ea094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "70da0182ce4251c190fa7c1edfcec377",
+    "url": "/static/media/vancouver_skyline_vec.70da0182.svg"
+  },
+  {
+    "revision": "973c9892b814a2ddf6490ea197ccde8b",
+    "url": "/static/media/mountains.973c9892.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10a64a7eb2eb6faaca6f",
+    "url": "/static/js/main.10a64a7e.chunk.js"
+  },
+  {
+    "revision": "5d8fc06694271968e9b9",
+    "url": "/static/js/1.5d8fc066.chunk.js"
+  },
+  {
+    "revision": "10a64a7eb2eb6faaca6f",
+    "url": "/static/css/main.5f9d365c.chunk.css"
+  },
+  {
+    "revision": "6d9cf5552869f17f0a3ca8c920847082",
+    "url": "/index.html"
+  }
+];
